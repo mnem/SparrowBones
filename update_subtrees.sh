a6bb86e59d552a6a47cca964fda69a775df66236
David Wagner <mnem@noiseandheat.com>
2012-02-25 19:11:52 +0000

    Script to update subtress because I always forget the command

---
+++ update_subtrees.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+git subtree pull --squash -P 4.2/source/Sparrow-Framework https://github.com/PrimaryFeather/Sparrow-Framework.git master
+git subtree pull --squash -P 4.1/source/Sparrow-Framework https://github.com/PrimaryFeather/Sparrow-Framework.git master
